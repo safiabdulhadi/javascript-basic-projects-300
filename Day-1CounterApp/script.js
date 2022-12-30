d76a087aed9a7ac5d7fa6b213f1e626bab1f12fe
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-12-30 17:24:18 +0100

    try event delegation too

--- Day-1CounterApp/script.js
+++ Day-1CounterApp/script.js
@@ -1,7 +1,45 @@
 const count = document.querySelector('.count');
-const add = document.querySelector('.add');
-const restCount = document.querySelector('.reset');
-const sub = document.querySelector('.subtract');
+// const add = document.querySelector('.add');
+// const restCount = document.querySelector('.reset');
+// const sub = document.querySelector('.subtract');
+
+const buttons = document.querySelector('.buttons');
+
+buttons.addEventListener('click', (e) =>{
+if(e.target.classList.contains('add')){
+// console.log('ADD');
+count.innerHTML++;
+// count.style.color = 'green';
+setColor();
+}
+if(e.target.classList.contains('subtract')){
+    count.innerHTML--;
+    // count.style.color = 'red';
+    setColor();
+}
+if(e.target.classList.contains('reset')){
+    count.innerHTML = 0;
+    count.style.color = 'white';
+    setColor();
+}
+});
+
+
+function setColor(){
+    if(count.innerHTML > 0){
+        count.style.color = 'green';
+    }else if(count.innerHTML < 0){
+        count.style.color = 'red';
+    }else{
+        color.style.color = 'white';
+    }
+}
+
+
+/*
+
+This is One way
+
 let countNumber = 0;
 
 add.addEventListener('click', () =>{
@@ -17,3 +55,5 @@ restCount.addEventListener('click', () =>{
     count.style.color = 'white';
 
 });
+
+*/
